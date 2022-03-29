7ebf827d0d9638d89f7213ed0aac85a4ce632ac5
Ozbey <gokhanozbey@gmail.com>
2022-03-28 21:38:33 -0400

    initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectSelfApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectSelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectSelfApplication.class, args);
+    }
+
+}
